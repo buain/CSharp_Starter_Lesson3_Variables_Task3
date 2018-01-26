6594006ede66e1d72ed9ddbb9c074ed8f194fc51
Vladimir <33662107+buain@users.noreply.github.com>
2018-01-26 17:51:51 +0300

    Initial commit

---
+++ Ploshad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploshad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.141;
+            double r = 5, s = 0;
+            s = pi * Math.Pow(r, 2);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
